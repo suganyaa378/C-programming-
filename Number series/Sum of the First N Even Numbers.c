0349226bffd385234d2034900206c1d976d3481b
suganyaa378 <suganyaa378@gmail.com>
2026-07-11 20:57:28 +0530

    Create Sum of the First N Even Numbers.c

---
+++ Number series/Sum of the First N Even Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, sum = 0;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        sum = sum + (2 * i);
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
