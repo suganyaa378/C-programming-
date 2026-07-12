c95eb9b53bd6b191b98bfde6c51744b397cbb46b
suganyaa378 <suganyaa378@gmail.com>
2026-07-12 20:35:38 +0530

    Create Sum of Prime Numbers up to N.c

---
+++ Number series/Sum of Prime Numbers up to N.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, count, sum = 0;
+
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i++)
+    {
+        count = 0;
+
+        for(j = 1; j <= i; j++)
+        {
+            if(i % j == 0)
+                count++;
+        }
+
+        if(count == 2)
+            sum = sum + i;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
