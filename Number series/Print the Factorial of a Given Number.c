8307d2ab2de3a3915ce55ad84267ca39f08dd8af
suganyaa378 <suganyaa378@gmail.com>
2026-07-12 20:39:40 +0530

    Create Print the Factorial of a Given Number.c

---
+++ Number series/Print the Factorial of a Given Number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    long long fact = 1;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("%lld", fact);
+
+    return 0;
+}
