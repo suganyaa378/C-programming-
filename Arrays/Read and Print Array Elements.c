c31bca43e3bc168f03c6c16246ed6d3e396638f9
suganyaa378 <suganyaa378@gmail.com>
2026-07-20 17:25:45 +0530

    Add program to read and print array elements

---
+++ Arrays/Read and Print Array Elements.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+    scanf("%d", &n);
+    for(i=0; i<n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Array elements are:\n");
+    for(i=0; i<n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
