5e833b8bf4ac9f44e8c7673032e8c458400f1a0c
suganyaa378 <suganyaa378@gmail.com>
2026-07-20 21:48:09 +0530

    Create Find the largest element in a matrix.c

---
+++ Matrix/Find the largest element in a matrix.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c, i, j, max;
+
+    scanf("%d%d",&r,&c);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    max = a[0][0];
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            if(a[i][j]>max)
+                max=a[i][j];
+
+    printf("Largest = %d",max);
+
+    return 0;
+}
