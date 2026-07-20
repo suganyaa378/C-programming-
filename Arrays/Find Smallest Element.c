0a1bc37aa933d5d7a24fd5b9f7c56b6ccd924d91
suganyaa378 <suganyaa378@gmail.com>
2026-07-20 17:29:42 +0530

    Add program to find the smallest element in an array

---
+++ Arrays/Find Smallest Element.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, min;
+
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++)
+        scanf("%d", &a[i]);
+
+    min = a[0];
+
+    for(i=1; i<n; i++)
+        if(a[i] < min)
+            min = a[i];
+
+    printf("Smallest = %d", min);
+
+    return 0;
+}
