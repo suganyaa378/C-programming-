00a4349ebb0cb4741c96f1c129789f015b6a9f21
suganyaa378 <suganyaa378@gmail.com>
2026-07-20 18:11:51 +0530

    Add program to separate even and odd elements

---
+++ Arrays/Separate Even and Odd Elements.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], even[100], odd[100];
+    int n, i, e = 0, o = 0;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < n; i++) {
+        if(a[i] % 2 == 0)
+            even[e++] = a[i];
+        else
+            odd[o++] = a[i];
+    }
+
+    printf("Even elements:\n");
+    for(i = 0; i < e; i++)
+        printf("%d ", even[i]);
+
+    printf("\nOdd elements:\n");
+    for(i = 0; i < o; i++)
+        printf("%d ", odd[i]);
+
+    return 0;
+}
